8f50742873ff6e55e5dbf108f26b84d52edcc39f
stg <a.sergiotorres@gmail.com>
2024-02-02 22:28:39 +0100

    on-off button works. blocks channel buttons. channel buttons work too.images loaded.  need to fix bug when turning off after programes

--- js/main.js
+++ js/main.js
@@ -1,21 +1,97 @@
 
-let imageMain = document.getElementById("Image")
+let image = document.getElementById("image") // PANTALLA
+let redBtn = document.getElementById("redBtn") // ON / OFF functions
 
-// ON / OFF functions
-let redBtn = document.getElementById("redBtn")
-let greenLight = document.getElementById("greenLight")
+
 
 // redBtn.onclick = changeColor;
+redBtn.addEventListener("click", (e) => {
+  let lastClass = image.classList[image.classList.length - 1];
 
-// function changeColor(imageMain){
-//     return imageMain.style.background = black;
-// } 
+  if (lastClass == "blackScreen") {
+    image.classList.remove("blackScreen")
+    image.classList.add("static")
+    // displayChannel.innerHTML = 'CHANNEL 1';
+    // setTimeout(function(){displayChannel.innerHTML = ''}, 2000);
+    // greenDot.style.backgroundColor= "lightgreen";
+  } else if (lastClass == "static") {
+    image.classList.remove(lastClass)
+    image.classList.add("blackScreen");
+    // greenDot.style.backgroundColor= "darkred";
+  }
+})
 
-redBtn.addEventListener("click", function() {
-    
-    redBtn.style.background = black;
-    greenLight.style.backgroundColor= darkred;
-  })
+
+
+// CHANNEL buttons
+// let btn1 = document.getElementById("btn1")
+// let btn2 = document.getElementById("btn2")
+// let btn3 = document.getElementById("btn3")
+// let btn4 = document.getElementById("btn4")
+// let btn5 = document.getElementById("btn5")
+// let btn6 = document.getElementById("btn6")
+
+
+const allBtns = document.getElementsByClassName("btn")
+let arrayBtns = Array.from(allBtns)
+
+const allNums = document.getElementsByClassName("numbers")
+let arrayNums = Array.from(allNums)
+
+arrayBtns.map(
+  item => item.addEventListener("click", (e) => {
+    let lastClass = image.classList[image.classList.length - 1];
+
+    if (lastClass != "blackScreen") {
+      switch (item.id) {
+        case "btn1":
+          image.classList.remove(lastClass)
+          image.classList.add("ch1");
+          break;
+        case "btn2":
+          image.classList.remove(lastClass)
+          image.classList.add("ch2");
+          break;
+        case "btn3":
+          image.classList.remove(lastClass)
+          image.classList.add("ch3");
+          break;
+        case "btn4":
+          image.classList.remove(lastClass)
+          image.classList.add("ch4");
+          break;
+      }
+    }
+  }
+))
+
+
+
+
+
+
+
+
+
+
+
+// greenDot.style.backgroundColor= "lightgreen";
+
+
+
+
+
+// let greenDot = document.getElementsByClassName("greenDot")
+
+
+
+
+
+
+
+
+
+
 
 // VOLUME functions
 let moreVolume = document.getElementById("moreVolume")
@@ -28,10 +104,28 @@ let displayTime = document.getElementById("displayTime")
 
 
 
-// CHANNEL functions
-let btn1 = document.getElementById("btn1")
-let btn2 = document.getElementById("btn2")
-let btn3 = document.getElementById("btn3")
-let btn4 = document.getElementById("btn4")
-let btn5 = document.getElementById("btn5")
-let btn6 = document.getElementById("btn6")
+
+
+
+
+// console.log(allNums)
+// console.log(arrayNums)
+
+// arrayBtns.map(
+//   el => {
+//     el.addEventListener("click", (e) => {
+//       image.classList.remove(lastClass)
+//       image.classList.add("ch" + e.target.)
+//       console.log(image.classList)
+//       console.log(e)
+//     })
+//   }
+// )
+
+// const allEffects = (ch) => {
+//   displayChannel.innerHTML = 'CHANNEL 1';
+//   setTimeout(function(){displayChannel.innerHTML = ''}, 2000)
+//   imageMain.classList.remove(lastClass)
+//   imageMain.classList.add(ch)
+
+// }
